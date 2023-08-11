903ad68cbc1a990f3afccbadc1224e9d21939dca
SACHIN SHARMA <117117742+DEVNINJASACHIN@users.noreply.github.com>
2023-08-12 04:10:28 +0530

    Add files via upload
    The WOIMpo Plugin is a powerful tool that allows you to import WooCommerce attributes and products from an API, create or insert categories, and provide license key security. It is designed to streamline the process of managing your WooCommerce store by automating data synchronization and ensuring the security of your plugin.
    
    Key Features:
    Import WooCommerce Attributes: Fetch attributes from an API and seamlessly import them into your WooCommerce store.
    
    Import WooCommerce Products: Retrieve products from an API and automatically import them into your WooCommerce catalog.
    
    Create or Insert Categories: Effortlessly create or insert categories from an API to better organize your product catalog.
    
    License Key Security: Ensure the security of your plugin by validating license keys through an API and enabling or disabling certain features accordingly.
    
    Advanced Functionality: Customize and extend the plugin's functionality to suit your unique business needs.
    
    User-Friendly Settings Page: Easily manage your license key and plugin settings through a user-friendly settings page in the WordPress admin dashboard.
    
    Scheduled Tasks: Set up automated tasks to import attributes, products, and categories on a daily basis, reducing manual effort.
    
    The WOIMpo Plugin simplifies the management of your WooCommerce store, saving you time and effort while ensuring data accuracy and security. Whether you're running a small online shop or managing a large e-commerce store, this plugin provides the necessary tools to enhance your productivity and streamline your operations.
    Plugin Information:
    
    Plugin Name: WOIMpo
    Version: 2.1
    Author: Sachin Sharma
    License: GPL2
    Get started with the WOIMpo Plugin today and unlock the full potential of your WooCommerce store!
    
    Note: This plugin was developed by Sachin Sharma and is actively maintained and supported. For more information and updates, please visit WOIMpo Plugin GitHub Repository.

---
+++ sachincode.php
@@ -0,0 +1,215 @@
+php
+<?php
+/*
+Plugin Name: WOIMpo
+Plugin URI: https://your-plugin-uri.com
+Description: Import WooCommerce attributes and products from API, create categories, and provide license key security.
+Version: 2.1
+Author: Sachin Sharma
+Author URI: https://devninjasachin.com
+License: GPL2
+*/
+
+//  Import WooCommerce attributes from API
+function import_woocommerce_attributes() {
+  // Make API request and retrieve attributes
+  $api_response = wp_remote_get('https://api.example.com/attributes');
+
+  if (is_wp_error($api_response)) {
+    // Handle API request error
+    error_log('Failed to retrieve attributes from API: ' . $api_response->get_error_message());
+    return;
+  }
+
+  $attributes = json_decode(wp_remote_retrieve_body($api_response), true);
+
+  if (!is_array($attributes)) {
+    // Handle invalid API response
+    error_log('Invalid API response for attributes: ' . wp_remote_retrieve_body($api_response));
+    return;
+  }
+
+  // Process the attributes and import into WooCommerce
+  foreach ($attributes as $attribute) {
+    // Import attribute logic
+  }
+}
+add_action('woimpo_import_woocommerce_attributes', 'import_woocommerce_attributes');
+
+// Import WooCommerce products from API
+function import_woocommerce_products() {
+  // Make API request and retrieve products
+  $api_response = wp_remote_get('https://api.example.com/products');
+
+  if (is_wp_error($api_response)) {
+    // Handle API request error
+    error_log('Failed to retrieve products from API: ' . $api_response->get_error_message());
+    return;
+  }
+
+  $products = json_decode(wp_remote_retrieve_body($api_response), true);
+
+  if (!is_array($products)) {
+    // Handle invalid API response
+    error_log('Invalid API response for products: ' . wp_remote_retrieve_body($api_response));
+    return;
+  }
+
+  // Process the products and import into WooCommerce
+  foreach ($products as $product) {
+    // Import product logic
+  }
+}
+add_action('woimpo_import_woocommerce_products', 'import_woocommerce_products');
+
+// Create or insert categories from API
+function create_or_insert_categories() {
+  // Make API request and retrieve categories
+  $api_response = wp_remote_get('https://api.example.com/categories');
+
+  if (is_wp_error($api_response)) {
+    // Handle API request error
+    error_log('Failed to retrieve categories from API: ' . $api_response->get_error_message());
+    return;
+  }
+
+  $categories = json_decode(wp_remote_retrieve_body($api_response), true);
+
+  if (!is_array($categories)) {
+    // Handle invalid API response
+    error_log('Invalid API response for categories: ' . wp_remote_retrieve_body($api_response));
+    return;
+  }
+
+  // Process the categories and create or insert into WooCommerce
+  foreach ($categories as $category) {
+    // Create or insert category logic
+  }
+}
+add_action('woimpo_create_or_insert_categories', 'create_or_insert_categories');
+
+// License key security feature
+function license_key_security() {
+  // Get the license key from the plugin settings
+  $license_key = get_option('woimpo_license_key');
+
+  // Make API request to validate the license key
+  $api_response = wp_remote_post('https://api.example.com/validate-license', array(
+    'body' => array(
+      'license_key' => $license_key,
+    ),
+  ));
+
+  if (is_wp_error($api_response)) {
+    // Handle API request error
+    error_log('Failed
+
+ to validate license key: ' . $api_response->get_error_message());
+    return;
+  }
+
+  $response_body = json_decode(wp_remote_retrieve_body($api_response), true);
+
+  if (!is_array($response_body) || !isset($response_body['valid'])) {
+    // Handle invalid API response
+    error_log('Invalid API response for license key validation: ' . wp_remote_retrieve_body($api_response));
+    return;
+  }
+
+  // Check the license key validation status
+  if ($response_body['valid']) {
+    // License key is valid, continue with plugin functionality
+
+    // Additional functionality for valid license key
+    additional_functionality();
+  } else {
+    // License key is invalid, disable certain plugin features
+    disable_functionality();
+  }
+}
+add_action('woimpo_license_key_security', 'license_key_security');
+
+// Additional functionality for valid license key
+function additional_functionality() {
+  // Implement your additional plugin functionality here
+  // This function will be called if the license key is valid
+  // Example: Enable advanced features or integrate with other plugins
+}
+add_action('woimpo_additional_functionality', 'additional_functionality');
+
+// Disable certain functionality for invalid license key
+function disable_functionality() {
+  // Implement disabling of certain plugin features here
+  // This function will be called if the license key is invalid
+  // Example: Disable specific features or display notifications
+}
+add_action('woimpo_disable_functionality', 'disable_functionality');
+
+// Custom action for advanced functionality
+function woimpo_advanced_functionality() {
+  // Implement your advanced functionality here
+  // Example: Perform complex data processing or integrate with external systems
+}
+add_action('woimpo_advanced_function', 'woimpo_advanced_functionality');
+
+// Add a settings page for the plugin
+function woimpo_settings_page() {
+  ?>
+  <div class="wrap">
+    <h1>WOIMpo Settings</h1>
+    <form method="post" action="options.php">
+      <?php settings_fields('woimpo_settings_group'); ?>
+      <?php do_settings_sections('woimpo_settings_section'); ?>
+      <?php submit_button(); ?>
+    </form>
+  </div>
+  <?php
+}
+
+// Register the settings and sections
+function woimpo_register_settings() {
+  // Register a settings group
+  register_setting('woimpo_settings_group', 'woimpo_license_key');
+
+  // Add a settings section
+  add_settings_section('woimpo_settings_section', 'License Key', 'woimpo_settings_section_callback', 'woimpo_settings');
+
+  // Add a field for the license key
+  add_settings_field('woimpo_license_key_field', 'License Key', 'woimpo_license_key_field_callback', 'woimpo_settings', 'woimpo_settings_section');
+}
+add_action('admin_init', 'woimpo_register_settings');
+
+// Callback function for the settings section
+function woimpo_settings_section_callback() {
+  echo '<p>Enter your license key to activate the plugin:</p>';
+}
+
+// Callback function for the license key field
+function woimpo_license_key_field_callback() {
+  $license_key = get_option('woimpo_license_key');
+  echo '<input type="text" name="woimpo_license_key" value="' . esc_attr($license_key) . '" />';
+}
+
+// Add the settings page to the WordPress menu
+function woimpo_add_settings_page() {
+  add_menu_page('WOIMpo Settings', 'WOIMpo', 'manage_options', 'woimpo_settings', 'woimpo_settings_page');
+}
+add_action('admin_menu', 'woimpo_add_settings_page');
+
+// Schedule import and category creation tasks
+
+
+function woimpo_schedule_tasks() {
+  if (!wp_next_scheduled('woimpo_import_woocommerce_attributes')) {
+    wp_schedule_event(time(), 'daily', 'woimpo_import_woocommerce_attributes');
+  }
+
+  if (!wp_next_scheduled('woimpo_import_woocommerce_products')) {
+    wp_schedule_event(time(), 'daily', 'woimpo_import_woocommerce_products');
+  }
+
+  if (!wp_next_scheduled('woimpo_create_or_insert_categories')) {
+    wp_schedule_event(time(), 'daily', 'woimpo_create_or_insert_categories');
+  }
+}
+add_action('wp', 'woimpo_schedule_tasks');
