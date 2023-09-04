9b3544af2e7a8df3a4b8cc581b3abfb3a4a412f7
SACHIN SHARMA <117117742+DEVNINJASACHIN@users.noreply.github.com>
2023-09-05 00:59:58 +0530

    Update Sachin-woimpo-basic.php

--- Sachin-woimpo-basic.php
+++ Sachin-woimpo-basic.php
@@ -193,3 +193,4 @@ function woimpo_add_settings_page() {
   add_menu_page('WOIMpo Settings', 'WOIMpo', 'manage_options', 'woimpo_settings', 'woimpo_settings_page');
 }
 add_action('admin_menu', 'woimpo_add_settings_page');
+>>
